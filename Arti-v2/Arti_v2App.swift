6a3b8755f4500718dae1c9950d6bea257da9d774
hilmyveradin <hilmyveradin@gmail.com>
2024-07-12 15:29:31 +0700

    Initial Commit

---
+++ Arti-v2/Arti_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Arti_v2App.swift
+//  Arti-v2
+//
+//  Created by Hilmy Veradin on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Arti_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
