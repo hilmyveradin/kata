30cebf11d3229cca2f6d2000b09af838e251fe57
hilmyveradin <hilmyveradin@gmail.com>
2024-07-12 16:18:56 +0700

    add secrets

---
+++ Arti-v2/SECRETS.swift
@@ -0,0 +1,10 @@
+//
+//  SECRETS.swift
+//  Arti-v2
+//
+//  Created by Hilmy Veradin on 12/07/24.
+//
+
+final class SECRETS {
+    static let OpenAIKey = "sk-proj-Wjev0S2EtTZ4ZuFwSaq0T3BlbkFJmoblhS4wg3Se5c7fNkdC"
+}
---
+++ Keyboard/SECRETS.swift
@@ -0,0 +1,12 @@
+//
+//  SECRETS.swift
+//  Keyboard
+//
+//  Created by Hilmy Veradin on 12/07/24.
+//
+
+import Foundation
+
+final class SECRETS {
+    static let OpenAIKey = "sk-proj-Wjev0S2EtTZ4ZuFwSaq0T3BlbkFJmoblhS4wg3Se5c7fNkdC"
+}
